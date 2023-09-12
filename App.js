5f015ce80c3735fd6fb85c4c471da6d90f8524cd
t1kyje00@students.oamk.fi <t1kyje00@students.oamk.fi>
2023-09-12 23:00:47 +0300

    TimeOut3000ms

--- App.js
+++ App.js
@@ -1,19 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, ActivityIndicator, View } from 'react-native';
 
 export default function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, [])
+    
+  
   return (
     <View style={[styles.container, styles.horizontal]}>
-      <ActivityIndicator size = 'small' color = 'red'/>
-      <ActivityIndicator size = 'large' color = 'blue' />
-      <ActivityIndicator size = 'extra-large' color = 'green' />
-      <ActivityIndicator size = 'small' color ='purple' />
-      <ActivityIndicator size = 'large' color = '#0000ff'/>
-      <ActivityIndicator size = 'extra-large' color = 'purple'/>
-      <ActivityIndicator size = 'small' color = 'green'/>
+      {isLoading &&
+      <ActivityIndicator size = 'small' color = 'red'animating={true} />}
+     {/* <ActivityIndicator size = 'large' color = 'blue' animating={true} />
+      <ActivityIndicator size = 'extra-large' color = 'green' animating={true} />
+      <ActivityIndicator size = 'small' color ='purple' animating={true}/>
+      <ActivityIndicator size = 'large' color = '#0000ff'animating={true}/>
+      <ActivityIndicator size = 'extra-large' color = 'purple'animating={true}/>
+      <ActivityIndicator size = 'small' color = 'green'animating={true}/>
       <ActivityIndicator size = 'large' />
-      <ActivityIndicator size = 'extra-large' color = 'purple'/>
+      <ActivityIndicator size = 'extra-large' color = 'purple'animating={true}/> 
+      POISTA ISLOADING && , USEEFFECT JA ANIMATING={TRUE}, JOS HALUAT AIKAKATKAISUN POIS KÄYTÖSTÄ*/}
+
     </View>
   );
 }
